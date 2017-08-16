07ba74de9933de382b5a0d5a74249aaf4849bb1b
DESKTOP-VKAFOB2\Guestnumber <lilith.retry@gmail.com>
2017-08-16 18:52:59 +0900

    課題15　標準クラスの文字数の取得を提出します。

---
+++ CampChallenge_java/015.標準クラス/Java15_2_2.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Java15class;
+
+/**
+ *
+ * @author Guestnumber
+ */
+public class Java15_2_2 {
+    public static void main(String[] args){
+// 自分のメールアドレスの「@」以降の文字を取得して、表示してください。
+    String mail = "lilith.retry@gmail.com";
+    System.out.print(mail.substring(12));
+    }
+}
